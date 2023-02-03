e1c25068453929970ca37df57c5f4c6292d9ff55
Fernando Basso <fernandobasso.br@gmail.com>
2023-02-03 16:53:11 -0300

    codetip: Refactor if !v to helper function

---
+++ src/js/if-set.js
@@ -0,0 +1,21 @@
+var log = console.log.bind(console);
+var err = console.error.bind(console);
+
+var { ENV, BRD, MKT, BRK} = process.env;
+
+log(ENV, BRD, MKT, BRK);
+
+function any(f, ...vars) {
+  return vars.some(f);
+}
+
+function isEmpty(v) {
+  return v === '' || v === undefined || v === null;
+}
+
+if (any(isEmpty, ENV, BRD, MKT, BRK)) {
+  err('Env vars not defined...')
+  process.exit(1);
+}
+
+log('EOT');
