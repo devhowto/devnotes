70090ff7d2dc9803a3e6b7a09d424b0d0de18d28
Fernando Basso <fernandobasso.br@gmail.com>
2023-02-26 09:08:04 -0300

    rb: Add notes on useful month details

--- src/rb/date/date_utils.rb
+++ src/rb/date/date_utils.rb
@@ -1,5 +1,67 @@
 require 'date'
 
+#
+# $ cal -y 2024
+#                                2024
+#
+#        January               February                 March
+# Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa
+#     1  2  3  4  5  6                1  2  3                   1  2
+#  7  8  9 10 11 12 13    4  5  6  7  8  9 10    3  4  5  6  7  8  9
+# 14 15 16 17 18 19 20   11 12 13 14 15 16 17   10 11 12 13 14 15 16
+# 21 22 23 24 25 26 27   18 19 20 21 22 23 24   17 18 19 20 21 22 23
+# 28 29 30 31            25 26 27 28 29         24 25 26 27 28 29 30
+#                                               31
+#         April                   May                   June
+# Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa
+#     1  2  3  4  5  6             1  2  3  4                      1
+#  7  8  9 10 11 12 13    5  6  7  8  9 10 11    2  3  4  5  6  7  8
+# 14 15 16 17 18 19 20   12 13 14 15 16 17 18    9 10 11 12 13 14 15
+# 21 22 23 24 25 26 27   19 20 21 22 23 24 25   16 17 18 19 20 21 22
+# 28 29 30               26 27 28 29 30 31      23 24 25 26 27 28 29
+#                                               30
+#         July                  August                September
+# Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa
+#     1  2  3  4  5  6                1  2  3    1  2  3  4  5  6  7
+#  7  8  9 10 11 12 13    4  5  6  7  8  9 10    8  9 10 11 12 13 14
+# 14 15 16 17 18 19 20   11 12 13 14 15 16 17   15 16 17 18 19 20 21
+# 21 22 23 24 25 26 27   18 19 20 21 22 23 24   22 23 24 25 26 27 28
+# 28 29 30 31            25 26 27 28 29 30 31   29 30
+#
+#        October               November               December
+# Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa   Su Mo Tu We Th Fr Sa
+#        1  2  3  4  5                   1  2    1  2  3  4  5  6  7
+#  6  7  8  9 10 11 12    3  4  5  6  7  8  9    8  9 10 11 12 13 14
+# 13 14 15 16 17 18 19   10 11 12 13 14 15 16   15 16 17 18 19 20 21
+# 20 21 22 23 24 25 26   17 18 19 20 21 22 23   22 23 24 25 26 27 28
+# 27 28 29 30 31         24 25 26 27 28 29 30   29 30 31
+#
+# Looking at the calendar for the year 2024 (which is a leap year, by
+# the way and thus February has 29 days, not 28), we can notice a few
+# things:
+#
+# • The first week of any given month can have from 1 to 7 days.
+# • The last week of any given month can also have from 1 to 7 days.
+# • There is never a first or last week of any given month with zero
+#   days, or more than seven r days 🤣.
+# • There are always between four and six weeks in any given month.
+#   Five weeks is more common. Sometimes a month spans six weeks.
+#   In more rare cases, if February is not a leap year (therefore it has
+#   28 days), and the first day of the month falls on Sunday, then the
+#   month will span for four weeks. 28 = 7 * 4.
+#
+#     $ cal 2 2015
+#         February 2015
+#     Su Mo Tu We Th Fr Sa
+#      1  2  3  4  5  6  7
+#      8  9 10 11 12 13 14
+#     15 16 17 18 19 20 21
+#     22 23 24 25 26 27 28
+#
+# Those tidbits are useful to keep in mind as they can provide ideas on
+# how to implement certain date-related utilities.
+#
+
 ##
 # A utility class for dates.
 #
