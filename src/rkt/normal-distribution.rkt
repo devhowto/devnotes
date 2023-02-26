8013b9bd4c631dac74cf37ae955ecb22254dbe35
Fernando Basso <fernandobasso.br@gmail.com>
2023-02-26 07:41:29 -0300

    rkt: Add normal distribution utility

---
+++ src/rkt/normal-distribution.rkt
@@ -0,0 +1,47 @@
+#lang racket
+
+;;
+;; See:
+;;
+;; • https://en.wikipedia.org/wiki/Normal_distribution
+;; • https://www.khanacademy.org/math/probability/xa88397b6:analyze-quantitative/normal-distributions-a2ii/v/ck12-org-normal-distribution-problems-empirical-rule
+;;
+
+;;;;
+;; Converts `n` to a string keeping two decimal places.
+;;
+(define (fmt n)
+  (real->decimal-string n 2))
+
+;;;;
+;; Generates the left values of a normal distribution number line.
+;;
+(define (left m d)
+  (let run ([n 4] [acc '()])
+    (cond
+      [(= n 0) acc]
+      [else
+       (run (sub1 n)
+            (append acc (list (fmt (- m (* d n))))))])))
+
+;;;;
+;; Generates the right values of a normal distribution number line.
+;;
+(define (right m d)
+  (let run ([n 4] [acc '()])
+    (cond
+      [(= n 0) acc]
+      [else
+       (run (sub1 n)
+            (cons (fmt (+ m (* d n))) acc))])))
+
+;;;;
+;; Generates a “normal distribution number line”.
+;;
+(define (normal-distribution mean standard-deviation)
+  (append (left mean standard-deviation)
+          (list mean)
+          (right mean standard-deviation)))
+
+
+(normal-distribution 10.4 1.9)
