3c72934391fad70c1f184d79de56f2404d871982
Fernando Basso <fernandobasso.br@gmail.com>
2023-03-27 07:56:22 -0300

    compfpjs: Add findColor() either example

---
+++ src/compfpjs/src/ex03b-either-color.js
@@ -0,0 +1,23 @@
+import {
+  log,
+} from './lib.js';
+
+/**
+ * @sig String -> String | undefined
+ */
+function findColor(name) {
+  return {
+    red: '#ff4444',
+    blue: '#3b5998',
+    yellow: '#fff68f',
+  }[name];
+}
+
+log(findColor('red'));
+//=> #ff4444'
+
+log(findColor('red').slice(1).toUpperCase());
+//=> 'FF4444'
+
+log(findColor('green').slice(1).toUpperCase());
+// TypeError: Cannot read properties of undefined (reading 'slice')
