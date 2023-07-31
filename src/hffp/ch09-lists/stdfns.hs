423d00319901335634386ad3c3052f20ce6fb38b
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-31 08:11:16 -0300

    haskell(hffp,ch09): Add some stdnfs

--- src/hffp/ch09-lists/stdfns.hs
+++ src/hffp/ch09-lists/stdfns.hs
@@ -1,85 +1,80 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+{-
+myAnd :: [Bool] -> Bool
+myAnd []       = True
+myAnd (b : bs) = case b of
+  False -> False
+  _     -> myAnd bs
 
 myAnd :: [Bool] -> Bool
-myAnd []     = True
-myAnd (x:xs) = x && myAnd xs
+myAnd []       = True
+myAnd (b : bs) = if b then myAnd bs else False
 
+-}
+
+myAnd :: [Bool] -> Bool
+myAnd []       = True
+myAnd (b : bs) = (&&) b (myAnd bs)
+-- myAnd = foldr (&&) True
 
 myOr :: [Bool] -> Bool
-myOr []     = False
-myOr (x:xs) = x || myOr xs
-
+myOr []       = False
+myOr (b : bs) = b || myOr bs
+-- myOr = foldr (||) False
 
 myAny :: (a -> Bool) -> [a] -> Bool
-myAny _ []     = False
-myAny f (x:xs) = f x || myAny f xs
+myAny _ []       = False
+myAny f (x : xs) = f x || myAny f xs
+--
+-- λ> myAny even [1, 3, 4, 5]
+-- True
+--
 
+-- myElem :: Eq a => a -> [a] -> Bool
+-- myElem _ []       = False
+-- myElem e (x : xs) = (==) e x || myElem e xs
+--
+-- λ> myElem 3 [7, 9, 3, 1, 3]
+-- True
+--
 
 myElem :: Eq a => a -> [a] -> Bool
-myElem _ []     = False
-myElem e (x:xs) = e == x || myElem e xs
-
-
-myElem' :: Eq a => a -> [a] -> Bool
-myElem' e = myAny (e ==)
-
+myElem e = myAny (e ==)
+--
+-- λ> myElem 3 [1, 5, 9]
+-- False
+--
+-- λ> myElem 3 [1, 5, 3, 9]
+-- True
+--
 
 myRev :: [a] -> [a]
-myRev []     = []
-myRev (x:xs) = myRev xs ++ [x]
+myRev []       = []
+myRev (x : xs) = myRev xs ++ [x]
+--
+-- λ> myRev "xyz"
+-- "zyx"
+--
 
-
+-- mySquish :: [[a]] -> [a]
+-- mySquish []         = []
+-- mySquish (xs : xss) = xs ++ mySquish xss
 squish :: [[a]] -> [a]
-squish []            = []
-squish (xs:listOfXs) = xs ++ squish listOfXs
-
+squish = foldr (++) []
+--
+-- λ> mySquish [[1], [2], [3]]
+-- [1,2,3]
+--
 
 squishMap :: (a -> [b]) -> [a] -> [b]
-squishMap _ []     = []
-squishMap f (x:xs) = f x ++ squishMap f xs
+squishMap _ [] = []
+squishMap f (xs : xss) = f xs ++ squishMap f xss
 --
--- λ> squishMap (\x -> "  __" ++ [x] ++ "__") "abc"
--- "  __a__  __b__  __c__"
+-- λ> squishMap (\x -> [1, x, 3]) [2]
+-- [1,2,3]
 --
-
-squishAgain :: [[a]] -> [a]
-squishAgain [] = []
-squishAgain listOfXs = squishMap (\xs -> xs ++ []) listOfXs
+-- λ> squishMap (\x -> [x + 1]) [1, 2, 3]
+-- [2,3,4]
 --
--- λ> squishAgain [[1..3], [4..6]]
--- [1,2,3,4,5,6]
---
-
-squishAgainPF :: [[a]] -> [a]
-squishAgainPF = squishMap (\xs -> xs ++ [])
---
--- λ> squishAgain [[1..3], [4..6]]
--- [1,2,3,4,5,6]
---
-
-
-myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
-myMaximumBy _ [x] = x
-myMaximumBy compFn (x:xs) =
-  case compFn x (myMaximumBy compFn xs) of
-    GT -> x
-    EQ -> x
-    LT -> myMaximumBy compFn xs
-
-
-myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
-myMinimumBy _ [x] = x
-myMinimumBy compFn (x:xs) =
-  case compFn x (myMinimumBy compFn xs) of
-    LT -> x
-    EQ -> x
-    GT -> myMinimumBy compFn xs
-
-
-myMaximum :: Ord a => [a] -> a
-myMaximum = myMaximumBy compare
-
-myMinimum :: Ord a => [a] -> a
-myMinimum = myMinimumBy compare
 
