706e134b02ff645a35534e7e48adc839924c2889
Fernando Basso <fernandobasso.br@gmail.com>
2023-08-03 09:26:34 -0300

    adocwiki: WIP move code to class

--- adocwiki/main.rb
+++ adocwiki/main.rb
@@ -1,64 +1,70 @@
+# rubocop:disable all
+
 require 'yaml'
 require 'fileutils'
 require 'asciidoctor'
 require 'erb'
 
-h = YAML.load_file('./nav.yml')
+class AdocWiki
+  def initialize(nav_file_path)
+    @nav_items = YAML.load_file(nav_file_path)
+  end
 
+  def conv(file)
+    rhtml = ERB.new(File.read('./adocwiki/templates/page.html.erb', mode: 'r:utf-8'))
 
-def conv(file)
-  rhtml = ERB.new(File.read('./adocwiki/templates/page.html.erb', mode: 'r:utf-8'))
+    arr = file.split('/')
 
-  arr = file.split('/')
+    # Drop last element, the file with the .adoc extension.
+    dirs = arr[0 .. -2]
 
-  # Drop last element, the file with the .adoc extension.
-  dirs = arr[0 .. -2]
+    # Prepend ‘build’ to the path.
+    dirs.insert(0, 'build')
 
-  # Prepend ‘build’ to the path.
-  dirs.insert(0, 'build')
+    outname = arr[-1]
 
-  outname = arr[-1]
+    FileUtils.mkpath(dirs.join('/'))
 
-  FileUtils.mkpath(dirs.join('/'))
+    adoc = Asciidoctor.load_file(
+      file,
+    )
 
-  adoc = Asciidoctor.load_file(
-    file,
-  )
+    html_page = rhtml.result(binding)
 
-  html_page = rhtml.result(binding)
+    File.write(
+      "#{dirs.join('/')}/#{outname.gsub(/adoc$/, 'html')}",
+      html_page,
+    )
 
-  File.write(
-    "#{dirs.join('/')}/#{outname.gsub(/adoc$/, 'html')}",
-    html_page,
-  )
+    p adoc.attributes['stem']
+  end
 
-  p adoc.attributes['stem']
-end
+  def do_level(nav_items = @nav_items)
+    if nav_items.class == Hash
+      puts 'obj is hash'
 
-def do_level(obj)
-  if obj.class == Hash
-    puts 'obj is hash'
-
-    obj.each_pair do |key, val|
-      if obj[key].class == Array
-        do_level(obj[key])
-      else
-        do_level(obj[key])
+      nav_items.each_pair do |key, val|
+        if nav_items[key].class == Array
+          do_level(nav_items[key])
+        else
+          do_level(nav_items[key])
+        end
       end
-    end
-  elsif obj.class == Array
-    puts 'obj is array'
+    elsif nav_items.class == Array
+      puts 'obj is array'
 
-    obj.each do |item|
-      if item.class == Array
-        do_level(item)
-      else
-        conv(item)
+      nav_items.each do |item|
+        if item.class == Array
+          do_level(item)
+        else
+          conv(item)
+        end
       end
+    else
+      puts 'what‽'
     end
-  else
-    puts 'what‽'
   end
 end
 
-do_level(h)
+
+AdocWiki.new('./nav.yml').do_level
