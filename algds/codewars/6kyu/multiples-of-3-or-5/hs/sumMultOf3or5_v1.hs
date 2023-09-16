6e56f630ea63f87b57397a02dc0f5bfe657d1614
Fernando Basso <fernandobasso.br@gmail.com>
2023-09-16 08:24:09 -0300

    algds: improve mult of 3 or 5

--- algds/codewars/6kyu/multiples-of-3-or-5/hs/sumMultOf3or5_v1.hs
+++ algds/codewars/6kyu/multiples-of-3-or-5/hs/sumMultOf3or5_v1.hs
@@ -4,8 +4,7 @@ multsOf3or5 :: [Int] -> [Int]
 multsOf3or5 = filter (\x -> rem x 3 == 0 || rem x 5 == 0)
 
 mults :: Int -> Int
-mults n = sum [xs | xs <- multsOf3or5 [1 .. n - 1]]
-
+mults n = sum . multsOf3or5 $ [1 .. n - 1]
 
 main :: IO ()
 main = do
