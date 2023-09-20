87823571d8aa6126dcc07aae3b0cf1c47256d80b
Fernando Basso <fernandobasso.br@gmail.com>
2023-09-20 10:08:56 -0300

    algds: WIP spin words

--- algds/codewars/6kyu/spin-words/c/spin_words_v1.c
+++ algds/codewars/6kyu/spin-words/c/spin_words_v1.c
@@ -6,18 +6,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Split a string. Save split parts in r and returns length
+ * of resulting array.
+ */
+char** str_split(const char* str, char* delim, short* len) {
+  *len = 0;
+  char* cpy = strdup(str);
+  char* tok = 0;
+  char** res = malloc(sizeof (char) * (strlen(cpy) / 2));
+
+  tok = strtok(cpy, delim);
+
+  while (tok != NULL) {
+    *(res + (*len)++) = strdup(tok);
+    tok = strtok(NULL, delim);
+  }
+
+  return res;
+}
+
 void spin_words(const char* s) {
+  // Copy so we own cpy mem.
   char* cpy = strdup(s);
-  char* tok = malloc(64);
-
-  tok = strtok(cpy, " ");
-
-  while (
-  printf("%s\n", tok);
 }
 
 int main(void) {
-  spin_words("hello my wonderful world");
+  short len;
+
+  char** res = str_split("Use the force, luke.", " ", &len);
+
+  printf("%d\n", len);
+
+  for (short i = 0; i < len; ++i)
+    printf("w: %s\n", *(res + i));
 
   return 0;
 }
