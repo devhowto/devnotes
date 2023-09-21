02e6c8abb5a22d22abeff85d9658b2afe622f5c9
Fernando Basso <fernandobasso.br@gmail.com>
2023-09-21 07:37:43 -0300

    algds(c): One solution for spin words

--- algds/codewars/6kyu/spin-words/c/spin_words_v1.c
+++ algds/codewars/6kyu/spin-words/c/spin_words_v1.c
@@ -6,40 +6,96 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * Split a string. Save split parts in r and returns length
- * of resulting array.
- */
-char** str_split(const char* str, char* delim, short* len) {
-  *len = 0;
-  char* cpy = strdup(str);
-  char* tok = 0;
-  char** res = malloc(sizeof (char) * (strlen(cpy) / 2));
 
-  tok = strtok(cpy, delim);
+void strdup(const char* src, char* dst) {
+  while ((*dst++ = *src++) != '\0')
+    ;
 
-  while (tok != NULL) {
-    *(res + (*len)++) = strdup(tok);
-    tok = strtok(NULL, delim);
-  }
-
-  return res;
+  *dst = '\0';
 }
 
-void spin_words(const char* s) {
+void str_rev(char* src, char* dst, short len) {
+  short i = 0;
+  short j = len - 1;
+
+  *(dst + (len - 1)) = '\0';
+
+  while (i < len)
+    *(dst + j--) = *(src + i++);
+}
+
+void spin_words(const char* s, char* r) {
   // Copy so we own cpy mem.
-  char* cpy = strdup(s);
+  char* cpy = 0;
+  char* tok = 0;
+  cpy = malloc(sizeof(s));
+  short cnt = 0;
+
+  strdup(s, cpy);
+
+  tok = strtok(cpy, " ");
+
+  while (tok != NULL) {
+    short l = strlen(tok);
+
+    if (l >= 5) {
+      char* rev = malloc(l);
+
+      str_rev(tok, rev, l);
+
+      if (cnt++ != 0)
+        strcat(r, " ");
+
+      strcat(r, rev);
+    }
+    else {
+      if (cnt++ != 0)
+        strcat(r, " ");
+
+      strcat(r, tok);
+    }
+
+    tok = strtok(NULL, " ");
+  }
 }
 
 int main(void) {
-  short len;
+  char* s0 = "Welcome";
+  char* w0 = malloc(strlen(s0));
+  char* s1 = "Use the force Luke";
+  char* w1 = malloc(strlen(s1));
+  char* s2 = "red hat hell";
+  char* w2 = malloc(strlen(s2));
+  char* s3 = "allocate enough memory";
+  char* w3 = malloc(strlen(s3));
 
-  char** res = str_split("Use the force, luke.", " ", &len);
+  spin_words(s0, w0);
+  spin_words(s1, w1);
+  spin_words(s2, w2);
+  spin_words(s3, w3);
 
-  printf("%d\n", len);
-
-  for (short i = 0; i < len; ++i)
-    printf("w: %s\n", *(res + i));
+  printf("\n");
+  printf("%s\n%s\n\n", s0, w0);
+  printf("%s\n%s\n\n", s1, w1);
+  printf("%s\n%s\n\n", s2, w2);
+  printf("%s\n%s\n\n", s3, w3);
 
   return 0;
 }
+
+//
+// $ make ./out/spin_words_v1 && ./out/spin_words_v1
+// gcc -std=c99 -Wall -pedantic -g -lc spin_words_v1.c -o out/spin_words_v1
+//
+// Welcome
+// emocleW
+//
+// Use the force Luke
+// Use the ecrof Luke
+//
+// red hat hell
+// red hat hell
+//
+// allocate enough memory
+// etacolla hguone yromem
+//
