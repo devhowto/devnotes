f7b4535254bec42e3fe5fc1b59ca5afac6120e14
Fernando Basso <fernandobasso.br@gmail.com>
2023-10-06 07:34:17 -0300

    algds(C): Finish rev_words() v1

--- src/c/strings/rev_words.c
+++ src/c/strings/rev_words.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <criterion/criterion.h>
 
-void rev_words(char *s, char* r) {
+/**
+ * Reverses the words in `s`.
+ *
+ * A word is any number of characters delimited by a space or
+ * the NULL terminator `\0`.
+ */
+void rev_words(char *s, char *r) {
   short cnt = 0, i, j;
 
   for (i = 0; s[i] != '\0'; ++i) {
@@ -15,27 +23,44 @@ void rev_words(char *s, char* r) {
 
     //
     // We are at the last char of the current word. Reverse it!
-    // reverse the current word.
     //
-
     for (j = 0; j < cnt; ++j)
       r[i + 1 - cnt + j] = s[i - j];
 
+    cnt = -1;
+
+    r[i + 1] = s[i + 1];
   }
 }
 
-// h e y   y o u
-//     •
+// int main(void) {
+//   char str[] = "hey you should know better by now";
+//   char res[56] = { '\0' };
+//
+//   rev_words(str, res);
+//
+//   printf("%s\n", res);
+//   return 0;
+// }
 
-// _ _ _
+void tester(char *str, char *rev);
 
-int main(void) {
-  char str[] = "hey you";
-  char res[8] = { '\0' };
+Test(rev_words, should_reverse_all_words) {
+  tester("Hello", "olleH");
+  tester("may the force", "yam eht ecrof");
+}
+
+void tester(char *str, char *expected) {
+  char res[96] = { '\0' };
 
   rev_words(str, res);
 
-  printf("%s\n", res);
-  return 0;
-}
+  if (strcmp(res, expected)) {
+    cr_assert_fail(
+      "String: %s\nReversed: %s\nExpeted: %s",
+              str,          res,     expected
+    );
+  }
 
+  cr_assert(1);
+}
