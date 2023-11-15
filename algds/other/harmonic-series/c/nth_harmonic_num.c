cdfdd68fad888bc5402af6dbc2df62a8de9c917c
Fernando Basso <fernandobasso.br@gmail.com>
2023-11-15 09:08:43 -0300

    algds(C): Refactor nth harmonic term

--- algds/other/harmonic-series/c/nth_harmonic_num.c
+++ algds/other/harmonic-series/c/nth_harmonic_num.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * Compute the harmonic series.
+ * Compute the nth harmonic number.
  *
  * ASSUME: n >= 1.
  */
-float nth_harmonic_num(short n) {
+double nth_harmonic_num(short n) {
   if (n == 1) return 1.0;
   return nth_harmonic_num(n - 1) + 1.0 / n;
 }
@@ -13,6 +13,7 @@ float nth_harmonic_num(short n) {
 int main(void) {
   printf("%.4f\n", nth_harmonic_num(4));
   printf("%.4f\n", nth_harmonic_num(8));
+  printf("%.4f\n", nth_harmonic_num(9));
 
   return 0;
 }
