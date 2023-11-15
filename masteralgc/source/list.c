d99f52d00ab4573f6213681f6953c61622b082a8
Fernando Basso <fernandobasso.br@gmail.com>
2023-11-15 12:11:29 -0300

    algds(C): WIP singly linked list

---
+++ masteralgc/source/list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+void list_init(List *list, void (*destroy)(void *data)) {
+  list->size = 0;
+  list->destroy = destroy;
+  list->head = NULL;
+  list->tail = NULL;
+
+  return;
+}
+
+void list_destroy(List *list) {
+  void *data;
+
+  //
+  // TO BE CONTINUED
+  //
+}
