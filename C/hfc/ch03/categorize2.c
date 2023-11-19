c7253fe5618b877cde7e16559285b4ada520148b
Fernando Basso <fernandobasso.br@gmail.com>
2023-11-19 11:42:12 -0300

    C(hfc): Format categorize2.c

--- C/hfc/ch03/categorize2.c
+++ C/hfc/ch03/categorize2.c
@@ -22,58 +22,44 @@
  *  $ cat the_rest.csv
  *
  */
-int main (int argc, char *argv[])
-{
-  /*
-   * Wee need to provide 5 args, but remember that argv[0]
-   * always contains the name of the program itself.
-   */
+int main(int argc, char *argv[]) {
+  /* Wee need 5 args, but remember that argv[0] contains the name of the
+   * program we are running. Therefore, we must check for 6. */
   if (argc != 6) {
-    fprintf (stderr,
-             "ERROR: You need to provide 5 arguments.\n");
+    fprintf(stderr, "ERROR: You need to provide 5 arguments.\n");
     return 1;
   }
 
   char line[80];
   FILE *in;
 
-  /*
-   * If we can't open the file, tell the user and
-   * exit with a non-zero status to indicate failure.
-   */
-  if (!(in = fopen ("spooky.csv", "r"))) {
-    fprintf (stderr, "Can't open “spooky.csv”\n");
+   /* If we can't open the file, inform the user about the problem and
+    * exit with a non-zero status to indicate failure. */
+  if (!(in = fopen("spooky.csv", "r"))) {
+    fprintf(stderr, "Can't open “spooky.csv”\n");
     return 1;
   }
 
+  /* Opening the streams could fail, but for simplicity we assume our
+   * fopen() calls jus work and we don't handle failure scenarios. */
+  FILE *file1 = fopen(argv[2], "w");
+  FILE *file2 = fopen(argv[4], "w");
+  FILE *file3 = fopen(argv[5], "w");
 
-  /*
-   * Not checking if we can create wite streams,
-   * but, let's assume we can.
-   */
-  FILE *file1 = fopen (argv[2], "w");
-  FILE *file2 = fopen (argv[4], "w");
-  FILE *file3 = fopen (argv[5], "w");
-
-  while (fscanf (in, "%79[^\n]\n", line) == 1) {
-    if (strstr (line, argv[1]))
-      fprintf (file1, "%s\n", line);
-    else if (strstr (line, argv[3]))
-      fprintf (file2, "%s\n", line);
+  while (fscanf(in, "%79[^\n]\n", line) == 1) {
+    if (strstr(line, argv[1]))
+      fprintf(file1, "%s\n", line);
+    else if (strstr(line, argv[3]))
+      fprintf(file2, "%s\n", line);
     else
-      fprintf (file3, "%s\n", line);
+      fprintf(file3, "%s\n", line);
   }
 
   /* REMEMBER: Always close the streams. */
-  fclose (in);
-  fclose (file1);
-  fclose (file2);
-  fclose (file3);
+  fclose(in);
+  fclose(file1);
+  fclose(file2);
+  fclose(file3);
 
   return 0;
 }
-
-
-/* vim: set syn=off ft=text ai tw=68 cc=+1,+10: */
-
-
