23ba4a6a13f2075fd9b200bc31d8e7118a65aaf2
Fernando Basso <fernandobasso.br@gmail.com>
2023-12-21 09:37:49 -0300

    exercism(C): Grains v2 with memoization

--- exercism/c/grains/grains.h
+++ exercism/c/grains/grains.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+uint64_t for_square(uint8_t n, uint8_t i, uint64_t t);
+uint64_t for_total(uint8_t i, uint64_t memo[], uint64_t acc);
 uint64_t square(uint8_t index);
 uint64_t total(void);
 
--- exercism/c/grains/grains_v2.c
+++ exercism/c/grains/grains_v2.c
@@ -1,64 +1,3 @@
-= Grains
-:page-subtitle: Exercism Easy Challenge
-:icons: font
-:toc: left
-:sectlinks:
-
-- link:https://exercism.org/tracks/c/exercises/grains[Grains Exercism Challenge^].
-
-== C
-
-=== Solution 1
-
-This solution is not very optimized.
-Some sort of memoization is probably possible.
-
-[source,c]
-----
-#include <stdint.h>
-#include "grains.h"
-
-/**
- * Counts how many grains there are in square idx.
- *
- * - T.C: O(n)
- * - S.C: O(1)
- */
-uint64_t square(uint8_t idx) {
-  uint64_t sq = 1;
-  uint8_t i;
-
-  if (idx < 1) return 0ull;
-
-  for (i = 1; i < idx; ++i)
-    sq += sq;
-
-  return sq;
-}
-
-/**
- * Counts the total grains in a 64 square board.
- *
- * - T.C: O(n²). To compute square 3, we recompute square 1 and 2.
- *               To compute square 4, we recompute square 3, 2 and 1.
- *               There is probably a way to memoize this.
- * - S.C: O(1)
- */
-uint64_t total(void) {
-  uint64_t t = 0ull;
-  uint8_t i;
-
-  for (i = 1; i <= 64; ++i)
-    t += square(i);
-
-  return t;
-}
-----
-
-=== Solution 2 with memoization
-
-[source,c]
-----
 #include <stdio.h>
 #include <stdint.h>
 #include "grains.h"
@@ -121,4 +60,3 @@ uint64_t total(void) {
 
   return for_total(1, memo, 0ull);
 }
-----
