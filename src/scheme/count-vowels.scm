d5278f0b5f66b18a82fb433159e1f712e9b5ddfb
Fernando Basso <fernandobasso.br@gmail.com>
2023-12-22 07:10:30 -0300

    algds(scm): Count vowels v1 and v2

--- src/scheme/count-vowels.scm
+++ src/scheme/count-vowels.scm
@@ -1,14 +1,21 @@
-(import srfi-13)
+(import (only srfi-13 string-fold string-index string-contains-ci))
 
 ;;;;
 ;; Count the number of vowels in s.
 ;;
-(define (count-vowels str)
+;(define (count-vowels str)
+;  (string-fold (lambda (c acc)
+;                 (if (or (string-index "aeiou" c)
+;                         (string-index "AEIOU" c))
+;                     (+ acc 1)
+;                     acc))
+;               0
+;               str))
+
+(define (count-vowels s)
   (string-fold (lambda (c acc)
-                 (if (or (string-index "aeiou" c)
-                         (string-index "AEIOU" c))
+                 (if (string-contains-ci "aeiou" (string c))
                      (+ acc 1)
                      acc))
                0
-               str))
-
+               s))
