3d16dacea4f28661e069b88b89764712bcdc4244
Fernando Basso <fernandobasso.br@gmail.com>
2024-01-08 07:11:55 -0300

    algds(py): More graph examples

---
+++ src/pyalgbook/ch02/graph_v4.py
@@ -0,0 +1,15 @@
+#
+# A graph in which the main collection is not a list, like the
+# previous examples, but a dict.
+#
+
+N = {
+        'a': set('bcdef'),
+        'b': set('ce'),
+        'c': set('d'),
+        'd': set('e'),
+        'e': set('f'),
+        'f': set('cgh'),
+        'g': set('fh'),
+        'h': set('fg')
+    }
