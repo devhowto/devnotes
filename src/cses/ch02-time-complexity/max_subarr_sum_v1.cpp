b0459cd5664e512f4bb791705b6fde486bee7025
Fernando Basso <fernandobasso.br@gmail.com>
2024-01-09 08:17:29 -0300

    algds(cpp): Max sub array sum v1

---
+++ src/cses/ch02-time-complexity/max_subarr_sum_v1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int find_max_sum(const int *xs, size_t len) {
+  int best = 0;
+
+  for (size_t i = 0; i < len; ++i) {
+    for (size_t j = i; j < len; ++j) {
+      int sum = 0;
+
+      for (size_t k = i; k <= j; ++k)
+        sum += *(xs + k);
+
+      best = max(best, sum);
+    }
+  }
+
+  return best;
+}
+
+int main(void) {
+  // int xs[] = { 1, 2 };
+  int xs[] = {-1, 2, 4, -3, 5, 2, -5, 2};
+
+  int max_sum = find_max_sum(xs, 8);
+
+  printf("%d\n", max_sum);
+  //=> 10
+
+  return 0;
+}
