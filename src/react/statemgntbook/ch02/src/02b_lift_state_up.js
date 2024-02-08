ec42ce1eeff70d1084299a296ae248eab0809f51
Fernando Basso <fernandobasso.br@gmail.com>
2024-02-08 07:49:01 -0300

    react(state): Lift state up rerender problem

---
+++ src/react/statemgntbook/ch02/src/02b_lift_state_up.js
@@ -0,0 +1,48 @@
+//
+// If multiple components must share the same state, one option is to
+// lift the state up to some parent component, and pass the state and
+// the function to set new state down as props.
+//
+// But what if a child component doesn't depend on state we are lifting
+// up? In this example, <AdditionalInfo /> doesn't need count and
+// setCount(), but it rerenders because the parent rerenders.
+//
+
+import { useState } from 'react';
+
+function Counter1({ count, setCount }) {
+  return (
+    <div className="counter-1">
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(c => c + 1)}>+1</button>
+    </div>
+  );
+}
+
+function Counter2({ count, setCount }) {
+  return (
+    <div className="counter-2">
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(c => c + 1)}>+1</button>
+    </div>
+  );
+}
+
+function AdditionalInfo() {
+  console.log("==== <AdditionalInfo /> render and rerender");
+  return <p>Additional Info</p>;
+}
+
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="counters-parent">
+      <AdditionalInfo />
+      <Counter1 count={count} setCount={setCount} />
+      <Counter2 count={count} setCount={setCount} />
+    </div>
+  );
+}
+
+export { Parent };
--- src/react/statemgntbook/ch02/src/App.js
+++ src/react/statemgntbook/ch02/src/App.js
@@ -1,6 +1,7 @@
 // import { AddOne } from './01b_local_state_num';
 // import { AddBase } from './01c_local_state_add_base';
-import { Parent } from './02a_lift_state_up';
+// import { Parent } from './02a_lift_state_up';
+import { Parent } from './02b_lift_state_up';
 
 import './App.css';
 
