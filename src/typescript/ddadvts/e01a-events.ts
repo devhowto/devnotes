637ebb5f854d3728878b8239d67d06fc1cea431f
Fernando Basso <fernandobasso.br@gmail.com>
2024-02-23 16:41:04 -0300

    ts: Update comments from not connected types example

--- src/typescript/ddadvts/e01a-events.ts
+++ src/typescript/ddadvts/e01a-events.ts
@@ -3,17 +3,28 @@ export const MODNAME = 'e01a-events';
 type Events = {
   add: string;
   remove: string;
+  ////
+  // Note this `move', which was added by someone at some time after the
+  // initial implementation. But the type and object below do not
+  // complain that `move` is missing.
+  //
   move: string;
 };
 
 type OnEvent = {
   onAdd: () => unknown;
   onRemove: () => unknown;
+  //
+  // Not complaining that `move` is not here.
+  //
 }
 
 const userActions: OnEvent = {
   onAdd: () => null,
   onRemove: () => null,
+  //
+  // Not complaining that `move` is not here.
+  //
 };
 
 /*
